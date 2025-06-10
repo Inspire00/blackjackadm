47d4435470d228a2b5fc58a6dc42b717fea13685
Vincent <you@example.com>
2025-06-10 14:37:19 +0200

    commit7

--- app/api/bookings/route.js
+++ app/api/bookings/route.js
@@ -3,45 +3,14 @@ import { db } from '../../lib/firebaseAdmin';
 import { NextResponse } from 'next/server';
 import { google } from 'googleapis';
 
-// Service account credentials (unchanged)
-const serviceAccount = {
-  client_email: "firebase-adminsdk-fbsvc@blackjack-8d304.iam.gserviceaccount.com",
-  private_key: `-----BEGIN PRIVATE KEY-----
-MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDFenuRhfbPwEkB
-LflCpjWqC/EbZodQn3prOsWaM0R9oMUSFHLNyxU4yiW1BwQrIHTiC6Q/It04Kjoc
-knSBKCfU1+ON3Ee3LZxF03ojQ+lCnMdsFCq4hhYVMHuUMTVceeICVXYCsniALLZK
-VaxGnAqhL+lf1IzRb2sd++SWEI5rYpu9gM/2a3ydb2FOrfmobIxxQkW1yoG/ZP6A
-Fwv8CfSbknQirK9v1jEiQMXp9O+dlRpIxuO6RuIaJKRp/uMwFi8iR9umv3E35Omw
-q/GNmLx0/KSEHKJr5Azssjtad4LCTNkrBpRn/vuZvnEtyPFb4rbt5c5+ZyFnaIIt
-C6H+f7EXAgMBAAECggEAEIhMuGxW5lU6h2iNTQDtmzSjiCy3a8HklZeQNXTQ6UeN
-WBJYLiLJvj0slpD/NPEWd/O+wDGWA/62nYlXsZYAB6sI3oNJ8B7xNBm2FVlSubeD
-k0746J8CuEScnYjYTWUMZvxqncn9M2KkfDXbiPaSH6TYMwqjVpR78XQS7GR3pKux
-MJBJHh5DTt/JNj1IiZU2Lmd+3m3m8Xtr2EZfQz5jcXvj4mAoEZ+k0akCwm01akp+
-+jsm6CvRaWrLqp1hylMDF+DLibNhBRVygoWE4fuJXRlLsY9weSV3ilqKUq2whqKd
-HSumJsm4bqWVZkgm7nSkJqaixpr+LfNgtdV9My/lGQKBgQDu9mCnwBlj/VLtBcUr
-C4VEY6wZGwPxkB4XgNmoaw+Vl3cgOkRlnNXskesYSUNwDcSSzOs/TvyuKE28xVXX
-oHu37lwzKBEvhpZ+tCFTM7yAlDNqZQD3wu7VwhKJzOQ7ddDpsrgbP36/e6kDz3y+
-HscSACZsCZuIJicMdDVduZnGAwKBgQDTju0Dzv2C488lNw2c5jBLm5XfSNpWsSiy
-2KbjXW02wVJsolUxgaQTpDqPHGNhLpmImQOOsFlP2sLzEv9iCxnFBEcGfWf2svk8
-v62OhP24EHpt8EPzWKRCAlWkVpnWrSyK6TIDzKdaWEa3PGzl88B0Dbp3KWB/e6gB
-I5MOqd+WXQKBgFR+b3zBMvklCJ5gjPAEedHNwHt8Du/qOomq+MtRV8hYLzoJAEhB
-pW6pxcvnk93aMGiL5p6TCkt2Ws5lpE3DJBvMP3OjNjhsyWfxv1rRD1UTkf6LykTQ
-+2w/3Bg3h3fy17TaibiwwdtpGUEMq5n5/f/CZ9OJriH9A4+mw3vpdZMLAoGBAL3W
-KuiX/PRO57pFRHneNMgjvMC35vST1CloL5kmZLIVl5jTsg7SfiEB5Wo11xeFcVAZ
-vg90PzBg5T6pdXDM9TrNM86nCXtd7jiM53hT7jgU0UeFqfPbPsncvmzRKQpyKt4b
-kz3FjwaK4CnclvthVvjMuN3kyyqaE4+yFGu8E14hAoGBAOkuwcMjhLS7Hvyx/bIr
-ZwArAYZ7Sd68EDgdMr+8Se2iDsyW90BGTN2RXzBWaIrJQc/jO+dHLBaBGBSngLeu
-cvdD1w4e0AmBewPh3JJE3UNYvJ/eoFkTJTDQkKduAZc5jbvY50D2B1ci1p2P/0u2
-PXLhFcxT5XgbCMOTLPxdLtzd
------END PRIVATE KEY-----`,
-};
+
 
 async function getAccessToken() {
   try {
     const jwtClient = new google.auth.JWT(
-      serviceAccount.client_email,
+      process.env.FIREBASE_CLIENT_EMAIL,
       null,
-      serviceAccount.private_key,
+      process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Handle newlines
       ['https://www.googleapis.com/auth/firebase.messaging']
     );
     const token = await jwtClient.authorize();
